b0c437cb97f69f349e3ca673ec400c5f26926441
Pwnicorn <pwnicorndev@gmail.com>
2014-10-28 23:11:08 +0100

    Remove Inkscape effect API, fixes #2

--- ctrl-cut.py
+++ ctrl-cut.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python
 
-import inkex, os, sys
+import os, sys
 from subprocess import call
 
-class CtrlCut(inkex.Effect):
-	def __init__(self):
-		inkex.Effect.__init__(self)
-
-	def effect(self):
-		svgfile = sys.argv[-1]
-		epsfile = svgfile+".eps"
-		null = open(os.devnull, "w")
-		call(["inkscape", svgfile, "--export-eps="+epsfile], stderr=null)
-		call(["ctrl-cut", epsfile], stderr=null)
-		null.close()
-		os.remove(svgfile)
-		os.remove(epsfile)
-
-if __name__ == '__main__':
-	effect = CtrlCut()
-	effect.affect()
+svgfile = sys.argv[-1]
+epsfile = svgfile+".eps"
+null = open(os.devnull, "w")
+call(["inkscape", svgfile, "--export-eps="+epsfile], stderr=null)
+call(["ctrl-cut", epsfile], stderr=null)
+null.close()
+os.remove(svgfile)
+os.remove(epsfile)
